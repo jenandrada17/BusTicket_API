7da16a4648d3cdb7d6d826933d910149501bf31f
jenandrada <jenandrada17@gmail.com>
2023-06-16 15:19:31 +0800

    first commit backend

---
+++ app.js
@@ -0,0 +1,94 @@
+import express from 'express'
+import { getUsers, getUser, createUser, loginUser, addDestination, 
+        getDestinations, deleteDestination, updatedestination } from './database.js'
+// import mysql from 'mysql2'
+
+const app = express()
+
+app.use(express.json()) 
+
+app.post("/userlogin", async (req, res) => {
+  const { Username, Password } = req.body
+  const user = await loginUser(Username, Password)
+
+  if (user) {
+    res.status(201).send("Successfully login!")
+  }  else {
+    res.status(401).send("Invalid username or password!")
+  }
+}) 
+
+app.get("/getusers", async (req, res) => {
+    const users = await getUsers()
+    res.send(users)
+})
+
+app.get("/getuserid/:id", async (req, res) => {
+    const id = req.params.id 
+    const user =
+    res.send(user)
+})
+
+app.post("/createuser", async (req, res) => {
+    const { Firstname, Lastname, Middlename, Position, Username, Password, ConfirmPassword } = req.body
+    const user = await createUser(Firstname, Lastname, Middlename, Position, Username, Password, ConfirmPassword)
+ 
+  if (user == "Username already Exists!") {  
+    res.status(401).send("Username already exist!") 
+  } else if (user == "Tha password don't match!") { 
+    res.status(401).send("Tha password don't match!") 
+  }  else {
+    res.status(201).send("User successfully added!")
+  } 
+
+}) 
+
+app.post("/adddestination", async (req, res) => { 
+  const { from_address, to_address, rate, note } = req.body
+  const destination = await addDestination( from_address, to_address, rate, note )
+
+if (destination == "Destination already exists!") {  
+  res.status(401).send(destination)   
+}  else {
+  res.status(201).send(destination)
+} 
+
+}) 
+
+app.get("/getdestinations", async (req, res) => {
+  const destinations = await getDestinations() 
+  res.send(destinations)
+}) 
+
+// app.get("/getDictinctAddressFrom", async (req, res) => {
+//   const destinations = await getDictinctAddressFrom() 
+//   res.send(destinations)
+// }) 
+
+// app.get("/getDictinctAddressTo", async (req, res) => {
+//   const destinations = await getDictinctAddressTo() 
+//   res.send(destinations)
+// }) 
+
+app.delete("/deletedestination/:desid", async (req, res) => { 
+  const destinations = await deleteDestination( req.params.desid ) 
+  res.send(destinations)
+}) 
+
+app.put("/updatedestination/:desid", async (req, res) => {  
+  const { desid } = req.params
+  const { from_address, to_address, rate, note } = req.body 
+
+  const destinations = await updatedestination( desid, from_address, to_address, rate, note ) 
+  res.send(destinations)
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000')
+})
+ 
---
+++ database.js
@@ -0,0 +1,113 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise()
+
+export async function getUsers() {
+    const [rows] = await pool.query("SELECT * FROM users") 
+    return rows
+}
+
+export async function getUser(id) {
+    const [rows] = await pool.query(`
+    SELECT * 
+    FROM users
+    where id = ?
+    `, [id]) 
+    return rows[0]
+}
+
+export async function createUser(Firstname, Lastname, Middlename, Position, Username, Password, ConfirmPassword) {  
+    let [resultss] = await pool.query(`SELECT * FROM users where username = ?`, [Username])     
+    console.log(resultss.length)
+    if ( resultss.length > 0 ) {   
+        return "Username already Exists!"
+    } else if ( Password !== ConfirmPassword ) {
+        return "Tha password don't match!" 
+    } else {   
+            const [result] = await pool.query(`
+            INSERT INTO Users (Firstname, Lastname, Middlename, Position, Username, Password)
+            VALUES (?, ?, ?, ?, ?, ?)
+            `, [Firstname, Lastname, Middlename, Position, Username, Password]) 
+
+            // console.log("Successfully Saved!") 
+            // const id = result.insertId
+            return "Successfully Saved!"
+    }
+}  
+
+export async function loginUser(Username, Password) { 
+    console.log(Username)
+    console.log(Password)
+    let [resultss] = await pool.query(`SELECT * FROM users where username = ? and password = ?`, [Username, Password])    
+    console.log(resultss.length)
+    if ( resultss.length > 0 ) {   
+
+        if (resultss[0].Username === Username && resultss[0].Password === Password) {
+            console.log("User Exists") 
+            return true 
+        }else{
+            console.log("User Doesn't Exists")
+            return false
+        }  
+
+    }  else {  
+        console.log("User Doesn't Exists")  
+        return false
+    } 
+}   
+
+export async function addDestination( From_Address, To_Address, Rate, Note ) {  
+    let [resultss] = await pool.query(`SELECT * FROM DESTINATIONS where from_address = ? and to_address = ?`, [From_Address, To_Address])     
+    if ( resultss.length > 0 ) {   
+        return "Destination already exists!"  
+    } else {   
+            const [result] = await pool.query(`
+            INSERT INTO DESTINATIONS ( from_address, to_address, rate, note )
+            VALUES (?, ?, ?, ?)
+            `, [ From_Address, To_Address, Rate, Note ]) 
+ 
+            return "Destination successfully added!"
+    }
+}  
+
+export async function getDestinations() {
+    const [rows] = await pool.query("SELECT * FROM destinations order by from_address, to_address")  
+    return rows
+}   
+
+// export async function getDictinctAddressFrom() {
+//     const [rows] = await pool.query("SELECT DISTINCT from_address FROM destinations order by from_address")  
+//     return rows
+// }  
+
+// export async function getDictinctAddressTo() {
+//     const [rows] = await pool.query("SELECT DISTINCT to_address FROM destinations order by to_address")  
+//     return rows
+// } 
+
+export async function deleteDestination( DesID ) {  
+    let [resultss] = await pool.query(`SELECT * FROM DESTINATIONS where desid = ?`, [DesID])     
+    if ( resultss.length > 0 ) {    
+        await pool.query(`DELETE FROM DESTINATIONS where desid = ?`, [DesID])
+        return "Successfully deleted!"
+    } else {
+        return "Destination ID does not exists!"
+    }
+}
+
+export async function updatedestination( DesID, From_Address, To_Address, Rate, Note ) {   
+    let [resultss] = await pool.query(`SELECT * FROM DESTINATIONS where desid = ?`, [DesID])      
+    if ( resultss.length > 0 ) {    
+        await pool.query(`UPDATE DESTINATIONS SET from_address = ?, to_address = ?, rate = ?, note = ? where desid = ?`, [From_Address, To_Address, Rate, Note, DesID])      
+        return "Successfully updated!"
+    } else {
+        return "Destination ID does not exists!"
+    }
+}
